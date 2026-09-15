a80d81717e6aad39642b80e4674b85f9cc8c8803
ShA-MS <57481825+pyraxxz@users.noreply.github.com>
2026-09-15 16:58:05 +0000

    feat(students): register lifecycle operations

--- src/modules/students/students.module.ts
+++ src/modules/students/students.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from '../../prisma.service';
 import { AuthModule } from '../auth/auth.module';
+import { StudentLifecycleController } from './student-lifecycle.controller';
+import { StudentLifecycleService } from './student-lifecycle.service';
 import { StudentsController } from './students.controller';
 import { StudentsService } from './students.service';
 
 @Module({
   imports: [AuthModule],
-  controllers: [StudentsController],
-  providers: [StudentsService, PrismaService],
-  exports: [StudentsService],
+  controllers: [StudentsController, StudentLifecycleController],
+  providers: [StudentsService, StudentLifecycleService, PrismaService],
+  exports: [StudentsService, StudentLifecycleService],
 })
 export class StudentsModule {}
