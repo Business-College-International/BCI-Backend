5a4e913ab6a5f3251f6ea4b8a4de74ce64f3f151
ShA-MS <57481825+pyraxxz@users.noreply.github.com>
2026-09-15 13:25:36 +0000

    feat(auth): register guardian profile module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Module } from '@nestjs/common';
 import { AcademicsModule } from './modules/academics/academics.module';
 import { ApplicationsModule } from './modules/applications/applications.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { GuardiansModule } from './modules/guardians/guardians.module';
 import { StudentsModule } from './modules/students/students.module';
 
 @Controller('health')
@@ -13,7 +14,7 @@ class HealthController {
 }
 
 @Module({
-  imports: [AuthModule, AcademicsModule, ApplicationsModule, StudentsModule],
+  imports: [AuthModule, AcademicsModule, ApplicationsModule, GuardiansModule, StudentsModule],
   controllers: [HealthController],
 })
 export class AppModule {}
