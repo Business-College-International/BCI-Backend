819cb4376ae7acba094a9191c471eb3a5f284d63
ShA-MS <57481825+pyraxxz@users.noreply.github.com>
2026-09-15 16:25:02 +0000

    feat: register announcements and stationery modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Module } from '@nestjs/common';
 import { AcademicReportsModule } from './modules/academic-reports/academic-reports.module';
 import { AcademicsModule } from './modules/academics/academics.module';
+import { AnnouncementsModule } from './modules/announcements/announcements.module';
 import { ApplicationsModule } from './modules/applications/applications.module';
 import { AttendanceModule } from './modules/attendance/attendance.module';
 import { AssessmentsModule } from './modules/assessments/assessments.module';
@@ -9,6 +10,7 @@ import { FinanceModule } from './modules/finance/finance.module';
 import { GuardiansModule } from './modules/guardians/guardians.module';
 import { PayrollModule } from './modules/payroll/payroll.module';
 import { StaffModule } from './modules/staff/staff.module';
+import { StationeryModule } from './modules/stationery/stationery.module';
 import { StudentsModule } from './modules/students/students.module';
 import { WalletModule } from './modules/wallet/wallet.module';
 
@@ -25,12 +27,14 @@ class HealthController {
     AuthModule,
     AcademicReportsModule,
     AcademicsModule,
+    AnnouncementsModule,
     ApplicationsModule,
     AttendanceModule,
     AssessmentsModule,
     GuardiansModule,
     PayrollModule,
     StaffModule,
+    StationeryModule,
     StudentsModule,
     FinanceModule,
     WalletModule,
