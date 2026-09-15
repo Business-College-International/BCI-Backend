bf2262a7c06ee35ae8eaeefb3335a5fba2b8e3e5
ShA-MS <57481825+pyraxxz@users.noreply.github.com>
2026-09-15 16:37:04 +0000

    chore(payroll): register management lifecycle

--- src/modules/payroll/payroll.module.ts
+++ src/modules/payroll/payroll.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PayrollController } from './payroll.controller';
 import { PayrollService } from './payroll.service';
+import { PayrollManagementController } from './payroll-management.controller';
+import { PayrollCalculatorService } from './payroll-calculator.service';
 
 @Module({
-  controllers: [PayrollController],
-  providers: [PayrollService],
+  controllers: [PayrollController, PayrollManagementController],
+  providers: [PayrollService, PayrollCalculatorService],
 })
 export class PayrollModule {}
