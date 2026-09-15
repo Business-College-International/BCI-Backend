c710542008ce1194fc9bcaf2ffaabcf8d4115be5
ShA-MS <57481825+pyraxxz@users.noreply.github.com>
2026-09-15 13:15:02 +0000

    security: provide shared authorization guards to applications

--- src/modules/applications/applications.module.ts
+++ src/modules/applications/applications.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from '../../prisma.service';
+import { AuthModule } from '../auth/auth.module';
 import { ApplicationsController } from './applications.controller';
 import { ApplicationsService } from './applications.service';
 
 @Module({
+  imports: [AuthModule],
   controllers: [ApplicationsController],
   providers: [ApplicationsService, PrismaService],
   exports: [ApplicationsService],
