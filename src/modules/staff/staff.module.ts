ba31336a1720e61e268cf93d4abaf06854ebe93e
ShA-MS <57481825+pyraxxz@users.noreply.github.com>
2026-09-15 17:51:12 +0000

    feat(staff): register teacher assignment management controller

--- src/modules/staff/staff.module.ts
+++ src/modules/staff/staff.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { StaffController } from './staff.controller';
+import { TeacherAssignmentManagementController } from './teacher-assignment-management.controller';
 import { StaffService } from './staff.service';
 
 @Module({
-  controllers: [StaffController],
+  controllers: [StaffController, TeacherAssignmentManagementController],
   providers: [StaffService],
 })
 export class StaffModule {}
