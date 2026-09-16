e53d06311988f5f9b12e1bbdef64818cf124a0e2
ShA-MS <57481825+pyraxxz@users.noreply.github.com>
2026-09-16 12:53:31 +0000

    Register timetable substitution validation

--- src/modules/timetable/timetable.module.ts
+++ src/modules/timetable/timetable.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TimetableController } from './timetable.controller';
 import { TimetableService } from './timetable.service';
+import { SubstitutionValidatorController } from './substitution-validator.controller';
+import { SubstitutionValidatorService } from './substitution-validator.service';
 
 @Module({
-  controllers: [TimetableController],
-  providers: [TimetableService],
+  controllers: [TimetableController, SubstitutionValidatorController],
+  providers: [TimetableService, SubstitutionValidatorService],
+  exports: [TimetableService, SubstitutionValidatorService],
 })
 export class TimetableModule {}
