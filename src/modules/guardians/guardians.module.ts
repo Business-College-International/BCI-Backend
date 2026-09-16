aa75da33224dbeda62d733075bcf11832bd8c215
ShA-MS <57481825+pyraxxz@users.noreply.github.com>
2026-09-16 15:52:22 +0000

    chore(guardians): register integrity service

--- src/modules/guardians/guardians.module.ts
+++ src/modules/guardians/guardians.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from '../../prisma.service';
 import { AuthModule } from '../auth/auth.module';
+import { GuardianIntegrityService } from './guardian-integrity.service';
 import { GuardiansController } from './guardians.controller';
 import { GuardiansService } from './guardians.service';
 
 @Module({
   imports: [AuthModule],
   controllers: [GuardiansController],
-  providers: [GuardiansService, PrismaService],
+  providers: [GuardiansService, GuardianIntegrityService, PrismaService],
   exports: [GuardiansService],
 })
 export class GuardiansModule {}
