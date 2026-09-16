b68f4c42d62d83b12809f03b882ff91711120021
ShA-MS <57481825+pyraxxz@users.noreply.github.com>
2026-09-16 10:16:38 +0000

    feat: register admission queue service

--- src/modules/applications/applications.module.ts
+++ src/modules/applications/applications.module.ts
@@ -3,11 +3,13 @@ import { PrismaService } from '../../prisma.service';
 import { AuthModule } from '../auth/auth.module';
 import { ApplicationsController } from './applications.controller';
 import { ApplicationsService } from './applications.service';
+import { ApplicationQueueController } from './application-queue.controller';
+import { ApplicationQueueService } from './application-queue.service';
 
 @Module({
   imports: [AuthModule],
-  controllers: [ApplicationsController],
-  providers: [ApplicationsService, PrismaService],
-  exports: [ApplicationsService],
+  controllers: [ApplicationsController, ApplicationQueueController],
+  providers: [ApplicationsService, ApplicationQueueService, PrismaService],
+  exports: [ApplicationsService, ApplicationQueueService],
 })
 export class ApplicationsModule {}
