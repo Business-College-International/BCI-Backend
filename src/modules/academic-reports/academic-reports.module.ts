b1a6526e75b25a4a1070f36c5b116f9d91b8a5ba
ShA-MS <57481825+pyraxxz@users.noreply.github.com>
2026-09-16 11:05:17 +0000

    Register report readiness module

--- src/modules/academic-reports/academic-reports.module.ts
+++ src/modules/academic-reports/academic-reports.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AcademicReportsController } from './academic-reports.controller';
+import { ReportReadinessController } from './report-readiness.controller';
 import { AcademicReportsService } from './academic-reports.service';
+import { ReportReadinessService } from './report-readiness.service';
 
 @Module({
-  controllers: [AcademicReportsController],
-  providers: [AcademicReportsService],
+  controllers: [AcademicReportsController, ReportReadinessController],
+  providers: [AcademicReportsService, ReportReadinessService],
 })
 export class AcademicReportsModule {}
