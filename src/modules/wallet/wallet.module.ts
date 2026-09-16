1ddd59e715e8554fda269ac3778e687761f1ac72
ShA-MS <57481825+pyraxxz@users.noreply.github.com>
2026-09-16 10:58:51 +0000

    feat(wallet): register wallet operations

--- src/modules/wallet/wallet.module.ts
+++ src/modules/wallet/wallet.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { WalletController } from './wallet.controller';
+import { WalletOperationsController } from './wallet-operations.controller';
 import { WalletService } from './wallet.service';
+import { WalletOperationsService } from './wallet-operations.service';
 
 @Module({
-  controllers: [WalletController],
-  providers: [WalletService],
+  controllers: [WalletController, WalletOperationsController],
+  providers: [WalletService, WalletOperationsService],
 })
 export class WalletModule {}
