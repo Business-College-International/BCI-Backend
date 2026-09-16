872e7f4477955f148c01f47bbf5134b99131c82e
ShA-MS <57481825+pyraxxz@users.noreply.github.com>
2026-09-16 13:18:10 +0000

    wire(payroll): register readiness controller

--- src/modules/payroll/payroll.module.ts
+++ src/modules/payroll/payroll.module.ts
@@ -3,9 +3,11 @@ import { PayrollController } from './payroll.controller';
 import { PayrollService } from './payroll.service';
 import { PayrollManagementController } from './payroll-management.controller';
 import { PayrollCalculatorService } from './payroll-calculator.service';
+import { PayrollReadinessController } from './payroll-readiness.controller';
+import { PayrollReadinessService } from './payroll-readiness.service';
 
 @Module({
-  controllers: [PayrollController, PayrollManagementController],
-  providers: [PayrollService, PayrollCalculatorService],
+  controllers: [PayrollController, PayrollManagementController, PayrollReadinessController],
+  providers: [PayrollService, PayrollCalculatorService, PayrollReadinessService],
 })
 export class PayrollModule {}
