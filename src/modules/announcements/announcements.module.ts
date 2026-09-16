16ac90f7600cc3d21b59af6d38eacaaac7a15342
ShA-MS <57481825+pyraxxz@users.noreply.github.com>
2026-09-16 12:03:53 +0000

    feat(announcements): register delivery operations

--- src/modules/announcements/announcements.module.ts
+++ src/modules/announcements/announcements.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { AnnouncementOperationsController } from './announcement-operations.controller';
+import { AnnouncementOperationsService } from './announcement-operations.service';
 import { AnnouncementsController } from './announcements.controller';
 import { AnnouncementsService } from './announcements.service';
 
 @Module({
-  controllers: [AnnouncementsController],
-  providers: [AnnouncementsService],
-  exports: [AnnouncementsService],
+  controllers: [AnnouncementsController, AnnouncementOperationsController],
+  providers: [AnnouncementsService, AnnouncementOperationsService],
+  exports: [AnnouncementsService, AnnouncementOperationsService],
 })
 export class AnnouncementsModule {}
