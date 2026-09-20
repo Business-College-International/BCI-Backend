78c83b505541402f2f61f640cb4cf429bed8486b
ShA-MS <57481825+pyraxxz@users.noreply.github.com>
2026-09-20 10:33:59 +0000

    feat(audit): attach request ids to audit log writes

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -1,8 +1,23 @@
 import { Injectable, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { getRequestId } from './common/request-context/request-context';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleDestroy {
+  constructor() {
+    super();
+
+    this.$use(async (params, next) => {
+      if (params.model === 'AuditLog' && params.action === 'create' && params.args?.data) {
+        const requestId = getRequestId();
+        if (requestId && !params.args.data.requestId) {
+          params.args.data.requestId = requestId;
+        }
+      }
+      return next(params);
+    });
+  }
+
   async onModuleDestroy(): Promise<void> {
     await this.$disconnect();
   }
